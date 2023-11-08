95946a1a46cdfeb743f431df8e607ec3b93c7536
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-11-08 17:14:34 +0300

    post endpoints

---
+++ innotter/post/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework.permissions import BasePermission
+
+from .models import Post
+
+
+class IsAdminOrIsOwnerOrIsModeratorOfTheOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        print("sss")
+        return bool(
+            request.user_data
+            and request.user_data.get("role") == "Admin"
+            or str(obj.page.owner) == request.user_data.get("uuid")
+            or request.user_data.get("role") == "Moderator"
+            and obj.page.owner_group_id == request.user_data.get("group_id")
+        )
--- innotter/post/views.py
+++ innotter/post/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .models import Post
+from .permissions import IsAdminOrIsOwnerOrIsModeratorOfTheOwner
+from .serializer import PostSerializer
+
+
+class PostViewsSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin, GenericViewSet):
+    queryset = Post.objects.all().prefetch_related("page")
+    serializer_class = PostSerializer
+    permission_classes_by_action = {
+        "update": [IsAdminOrIsOwnerOrIsModeratorOfTheOwner],
+        "partial_update": [IsAdminOrIsOwnerOrIsModeratorOfTheOwner],
+        "destroy": [IsAdminOrIsOwnerOrIsModeratorOfTheOwner],
+    }
+
+    def get_permissions(self):
+        try:
+            return [
+                permission()
+                for permission in self.permission_classes_by_action[self.action]
+            ]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
