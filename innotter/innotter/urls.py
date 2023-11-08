99dd71434486bc3438b885a94dda9e39024b9ae9
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-11-08 17:16:25 +0300

    urls

--- innotter/innotter/urls.py
+++ innotter/innotter/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("page.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("page.urls")),
+    path("", include("post.urls")),
+    path("", include("tag.urls")),
+]
