2b4c58b56836de09652622b53aeea54f78393932
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-11-15 10:12:01 +0300

    update value_serializer

--- innotter/innotter/producer.py
+++ innotter/innotter/producer.py
@@ -1,3 +1,5 @@
+import json
+
 from kafka import KafkaProducer
 
 from .settings import env
@@ -7,7 +9,7 @@ class KafkaProducerWrapper:
     def __init__(self):
         self.producer = KafkaProducer(
             bootstrap_servers=env("KAFKA_BOOTSTRAP_SERVERS"),
-            value_serializer=lambda v: str(v).encode("utf-8"),
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
         )
 
     def produce_message(self, topic, message):
